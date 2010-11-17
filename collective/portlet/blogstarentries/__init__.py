ce7127bae61435026fbdc1ba3e95d01d68ff859a
micecchi <micecchi@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-11-17 15:44:56 +0000

    initial import

---
+++ collective/portlet/blogstarentries/__init__.py
@@ -0,0 +1,5 @@
+from zope.i18nmessageid import MessageFactory
+BlogstarLastEntriesMessageFactory = MessageFactory('collective.portlet.blogstarentries')
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.0'
+
+setup(name='collective.portlet.blogstarentries',
+      version=version,
+      description="An additional portlet that show the last entries of a blog, outside the effective blog folder",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.net',
+      url='http://plone.org',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      """,
+      )
